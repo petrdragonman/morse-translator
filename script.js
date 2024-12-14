a90194f03112e9e62262fff5fb1e2804f0b500b5
petrdragonman <petrdragonman@me.com>
2024-12-15 10:01:13 +1100

    removes old comments and extra unused code

--- script.js
+++ script.js
@@ -1,7 +1,5 @@
 import data from "./assets/morseCode.json" with {type: "json"};
-//import { fromEnglishToMorse } from "./Maths/fromEnglishToMorse.js";
 import { translateEnglishToMorse } from "./Maths/fromEnglishToMorse.js";
-//import { fromMorseToEnglish } from "./Maths/fromMorseToEnglish.js";
 import { translateMorseToEnglish } from "./Maths/fromMorseToEnglish.js";
 
 
@@ -20,7 +18,6 @@ englishInputElement.addEventListener("input", (event) => {
   translateEnglishToMorse(event.target.value);
 });
 
-//////////////////////////////////////////////////
 const morseInputElement = document.getElementById("morse");
 morseInputElement.addEventListener("input", (event) => {
   if(!MORSEREX.test(event.target.value)) {
@@ -28,54 +25,10 @@ morseInputElement.addEventListener("input", (event) => {
   }
   translateMorseToEnglish(event.target.event);
 });
-////////////////////////////////////////////////////
-// const morseInputElement = document.getElementById("morse").addEventListener("input", (event) => {
-//   if(!MORSEREX.test(event.target.value)) {
-//     event.target.value = event.target.value.replace(/[^.-\s]/g, "");
-//   }
-//   translateMorseToEnglish(event.target.event);
-// });
-// morseInputElement();
 
-//////////////////////////////////
-//fromEnglishToMorse(text); ///////////////////////////////////////////////////////////////////////////////////////////////
-// const fromEnglishToMorse = (text) => {
-//   // to upper case -> split to create array with chars -> map to find coresponding morse letter -> join with space
-//   return text.toUpperCase().split("").map((char) => data[char]).join(" ");
-// };
-//////////////////////////////////
-
-///////////////////////////////////////////
 translateEnglishToMorse();
-// const translateEnglishToMorse = () => {
-//   const inputText = document.getElementById('english').value;
-//   const outputText = document.getElementById('morse');
-  
-//   // regex expression for alphabet only + space + .
-//   if(inputText) {
-//     const translatedText = fromEnglishToMorse(inputText);
-//     // Display translated text
-//     outputText.value = translatedText;
-//   }
-// };
-//////////////////////////////////////////////
-translateMorseToEnglish();
-// const translateMorseToEnglish = () => {
-//   const inputText = document.getElementById("morse").value;
-//   const outputText = document.getElementById("english");
-//   // regex
-//   const translatedText = fromMorseToEnglish(inputText);
-//   outputText.value = translatedText;
-// };
-//////////////////////////////////////////////
 
-//fromMorseToEnglish(text); //////////////////////////////////////////////////////////////////////////////////////////////////
-////////////////////////////////////////
-// const fromMorseToEnglish = (text) => {
-//   return text.split(" ").map((code) => Object.keys(data).find(key => data[key] === code)).join("");
-//   // ".- .. .-. ....... - . -- .--. . .-. .- - ..- .-. ."; // air remperature
-// };
-///////////////////////////////////
+translateMorseToEnglish();
 
 // to clear the textarea
 const clearTheTextArea = document.getElementById("btn").addEventListener("click", () => {
@@ -85,50 +38,3 @@ const clearTheTextArea = document.getElementById("btn").addEventListener("click"
   outputText.value = "";
   inputText.value = "";
 });
-
-/*
-morse example
-.... . .-.. .-.. ---   .-- --- .-. .-.. -.. --..--   - .... .. ...   .. ...   .-   -- --- .-. ... .   -.-. --- -.. .   . -..- .- -- .--. .-.. .
-
-Breakdown:
-H = ....
-E = .
-L = .-..
-L = .-..
-O = ---
-(space between words)
-W = .--
-O = ---
-R = .-.
-L = .-..
-D = -..
-(space)
-T = -
-H = ....
-I = ..
-S = ...
-(space)
-I = ..
-S = ...
-(space)
-A = .-
-(space)
-M = --
-O = ---
-R = .-.
-S = ...
-E = .
-(space)
-C = -.-.
-O = ---
-D = -..
-E = .
-(space)
-E = .
-X = -..-
-A = .-
-M = --
-P = .--.
-L = .-..
-E = .
-*/
